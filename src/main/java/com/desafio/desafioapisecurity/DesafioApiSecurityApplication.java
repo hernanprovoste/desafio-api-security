b6eb0e0868c64244bf314fbcac51981eab5f4210
Hernan Provoste <hernanprovostev@outlook.com>
2023-11-28 22:37:50 -0300

    first commit

---
+++ src/main/java/com/desafio/desafioapisecurity/DesafioApiSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.desafioapisecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/desafioapisecurity/DesafioApiSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.desafioapisecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
